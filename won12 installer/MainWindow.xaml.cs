f15830dc4cee5ee1e3638cf5dd06bf019ccd354b
snooze <89538185+snooze789@users.noreply.github.com>
2022-07-30 17:28:45 -0400

    Add project files.

---
+++ won12 installer/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace won12_installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            real_win12.Visibility = Visibility.Hidden;
+            buttonlol.Visibility = Visibility.Hidden;
+            destroyed.Visibility = Visibility.Visible;
+        }
+    }
+}
